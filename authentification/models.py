71fa0c02b15341408563ac7f16cdc9c1b372b132
LioExpleo <lionel.rocheteau@expleogroup.com>
2022-12-29 01:00:09 +0100

    ajout requirements

--- authentification/models.py
+++ authentification/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    # username = None     # Nom utilisateur
+    first_name = None   # prénom
+    last_name = None    # nom de famille
+    email = None        # email
+    # password = None     # password
+    is_staff = None     # utilisateur pouvant se connecter au site admin django
+    is_active = False   # utilisateur actif
+    is_superuser = None # obtention de toutes les permissions, telles que l'accès au site admin
+
+    pass
